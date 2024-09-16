7bdb5bb4bc2dfcd56091249e51e0e63158dc0075
Cassie Cheung <me@soopy.moe>
2024-09-16 12:55:37 +0800

    chore: i did not cook

---
+++ AST10106/src/moe/soopy/ast10106/lab1/HelloWorld.java
@@ -0,0 +1,27 @@
+// I managed to lose my original source files so this is a re-implementation (copy of slides) of what I did.
+
+/*
+ * This is a simple hello world program with some arithmetic operations.
+ */
+
+package moe.soopy.ast10106.lab1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		HelloWorld.basicArithmetics();
+	}
+	
+	static void basicArithmetics() {
+		int sum = 0;
+		int int1, int2;
+		
+		int1 = 10;
+		int2 = 20;
+		
+		sum = int1 + int2;
+		System.out.println(String.format("The result of %s + %s = %s", int1, int2, sum));
+	}
+
+}
