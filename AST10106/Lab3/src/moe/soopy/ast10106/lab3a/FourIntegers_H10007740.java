bdf978f1e9c9f93c3e551fd28cd2c134d23fd135
Cassie Cheung <hwc39103@uowmail.edu.au>
2024-09-27 17:01:18 +0800

    feat(ast10106): complete lab3{a,b}
    Signed-off-by: Cassie Cheung <hwc39103@uowmail.edu.au>

--- AST10106/Lab3/src/moe/soopy/ast10106/lab3a/FourIntegers_H10007740.java
+++ AST10106/Lab3/src/moe/soopy/ast10106/lab3a/FourIntegers_H10007740.java
@@ -1,5 +1,30 @@
+/**
+ * This simple program asks the user 4 integers and averages them in 4 decimal points.
+ * @author H10007740
+ * 
+ */
+
 package moe.soopy.ast10106.lab3a;
 
-public class FourIntegers_H10007740 {
+import java.util.Scanner;
 
+public class FourIntegers_H10007740 {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please input 4 integers on each line:");
+		
+		// This could be better done with using an array but we haven't reached that point yet.
+		float num1 = scanner.nextFloat();
+		float num2 = scanner.nextFloat();
+		float num3 = scanner.nextFloat();
+		float num4 = scanner.nextFloat();
+		
+		// close the scanner after use.
+		scanner.close();
+		
+		// do the calculation
+		float average = (num1 + num2 + num3 + num4) / 4; // is there a built-in function to do this? not that i'd use this now
+		System.out.printf("You have provided the numbers: %.1f, %.1f, %.1f, %.1f\n", num1, num2, num3, num4);
+		System.out.printf("The average of the numbers is: %.4f\n", average);
+	}
 }
